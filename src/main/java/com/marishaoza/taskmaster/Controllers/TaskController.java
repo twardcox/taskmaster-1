8f227c511db377c3cd56068f1f00f387053eac8f
MarishaHoza <marisha.hoza@gmail.com>
2019-09-11 10:59:16 -0700

    new routes

--- src/main/java/com/marishaoza/taskmaster/Controllers/TaskController.java
+++ src/main/java/com/marishaoza/taskmaster/Controllers/TaskController.java
@@ -1,6 +1,7 @@
 package com.marishaoza.taskmaster.Controllers;
 
 
+import com.marishaoza.taskmaster.Models.HistoryObj;
 import com.marishaoza.taskmaster.Models.Task;
 import com.marishaoza.taskmaster.Repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,7 @@ public class TaskController {
         newTask.setTitle(task.getTitle());
         newTask.setDescription(task.getDescription());
         newTask.setStatus("Available");
+        newTask.addHistory(new HistoryObj("Task created"));
         taskRepository.save(newTask);
         return newTask;
     }
@@ -41,7 +43,25 @@ public class TaskController {
             taskToUpdate.setStatus("Accepted");
         } else if (taskToUpdate.getStatus().equals("Accepted")) {
             taskToUpdate.setStatus("Finished");
+        } else if (taskToUpdate.getStatus().equals("Finished")) {
+            return taskToUpdate;
         }
+        taskToUpdate.addHistory(new HistoryObj("--> " + taskToUpdate.getStatus()));
+        taskRepository.save(taskToUpdate);
+        return taskToUpdate;
+    }
+
+    @GetMapping("/users/{name}/tasks")
+    public List<Task> getTasksByAssignee(@PathVariable String name) {
+        return (List) taskRepository.findAllByAssignee(name).get();
+    }
+
+    @PutMapping("/tasks/{id}/assign/{assignee}")
+    public Task updateTaskAssignee(@PathVariable String id, @PathVariable String assignee) {
+        Task taskToUpdate = taskRepository.findById(id).get();
+        taskToUpdate.setAssignee(assignee);
+        taskToUpdate.setStatus("Assigned");
+        taskToUpdate.addHistory(new HistoryObj("--> Assigned to " + assignee));
         taskRepository.save(taskToUpdate);
         return taskToUpdate;
     }
